5b72a1b299d632dfb3037608b68d72196f0982a6
Sumit <kumar.sumit2512@gmail.com>
2024-08-18 16:48:03 +0530

    2nd commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+import styles from "./index.css"
+export default function App() {
+  return (
+    <div className='text-3xl font-bold text-red-500'>
+      hello
+    </div>
+  )
+}
